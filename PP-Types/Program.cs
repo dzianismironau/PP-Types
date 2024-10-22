a8b235c4a38421e006534cdb0037bad596104cea
dzianismironau <denis.mironow2@gmail.com>
2024-10-22 16:32:25 +0200

    Lab-2

--- PP-Types/Program.cs
+++ PP-Types/Program.cs
@@ -14,9 +14,15 @@ string[] names = {
 // After last element should be ".".
 void PrintGroups(string[] t, int perLine)
 {
-
-    // Write required code.
-
+    for (int i = 0; i < t.Length; i++)
+    {
+        Console.Write(t[i]);
+        if (i < t.Length - 1)
+            Console.Write(", ");
+        if ((i + 1) % perLine == 0 && i < t.Length - 1)
+            Console.WriteLine();
+    }
+    Console.WriteLine(".");
 }
 
 
@@ -27,9 +33,15 @@ void PrintGroups(string[] t, int perLine)
 
 void PrintColumns(string[] t, int perLine, int width)
 {
+    for (int i = 0; i < t.Length; i++)
+    {
+        Console.Write(t[i].PadRight(width).Substring(0, width));
 
-    // Write required code.
-
+        if ((i + 1) % perLine == 0)
+            Console.WriteLine();
+        else
+            Console.Write("|");
+    }
 }
 
 
